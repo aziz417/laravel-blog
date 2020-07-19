a2856f71586ff0cc1e8a69043d061e3635ef4de2
Fictionsoft <fictionsoft50@gmail.com>
2020-07-19 21:10:34 +0600

    show comment by author complete

--- app/Http/Controllers/Author/ExtraController.php
+++ app/Http/Controllers/Author/ExtraController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Author;
 
 use App\Http\Controllers\Controller;
+use App\Model\Comment;
 use Illuminate\Http\Request;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Auth;
@@ -26,5 +27,24 @@ class ExtraController extends Controller
         Toastr::success('Your this post remove to favorite list is successfully', 'Success', ["positionClass" => "toast-top-right"]);
         return redirect()->route('author.favorites.index');
 
+    }
+
+    public function commentIndex(){
+        $user = Auth::id();
+        $comments = Comment::where('user_id', $user)->orderBy('id', 'DESC')->get();
+        return view('backend.comment.index', compact('comments'));
+    }
+
+    public function commentDestroy($id){
+
+        $comment = Comment::findOrFail($id);
+        if($comment->user_id == Auth::id()){
+            $comment->delete();
+            Toastr::success('Comment delete successfully', 'Success', ["positionClass" => "toast-top-right"]);
+            return redirect()->back();
+        }else{
+            Toastr::success('You can not delete this Comment', 'Success', ["positionClass" => "toast-top-right"]);
+            return redirect()->back();
+        }
     }
 }
